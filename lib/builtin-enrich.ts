b6a70c0f8c3abda941972416230908aef3331044
marinsdigital28-prog <marinsdigital28@gmail.com>
2026-08-31 11:57:15 -0300

    data: parte 2 + índice das 35 fichas cadastrais embutidas

--- lib/builtin-enrich.ts
+++ lib/builtin-enrich.ts
@@ -1,1 +1,3 @@
-export const BUILTIN_ENRICH = [] as const;
+import { ENRICH_PART1 } from './builtin-enrich-part1';
+import { ENRICH_PART2 } from './builtin-enrich-part2';
+export const BUILTIN_ENRICH = [...ENRICH_PART1, ...ENRICH_PART2];
